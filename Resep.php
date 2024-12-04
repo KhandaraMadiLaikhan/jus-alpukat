d52591f051947a3ddf9c36e99ac92b9ac6123a5f
RimaSitiNurhalimah <rimasitinurhalimah20@gmai>
2024-12-04 14:54:48 +0700

    inisiasi pbo-jus-alpukat versi 2

--- Resep.php
+++ Resep.php
@@ -6,3 +6,12 @@ echo "1. Alpukat";
 echo "2. Es Batu";
 echo "3. Susu Putih";
 echo "4. Gula";
+
+//Rima siti nurhalimah
+echo "Alat Jus Alpukat";
+echo "Pisau";
+echo "Blender";
+echo "Gelas";
+echo "Gunting";
+echo "Sendok";
+echo "Sedotan";

23b1e594e130738c764748ac7f6caf2c03417310
KhandaraMadiLaikhan <09khandara@gmail.com>
2024-12-05 12:27:12 +0700

    delete baris

--- Resep.php
+++ Resep.php
@@ -19,7 +19,6 @@ echo "Gunting";
 echo "Sendok";
 echo "Sedotan";
 
-
 //Dimas Adi Nugarha
 echo "Belah alpukat, ambil dagingnya dengan sendok.";
 echo "Masukkan alpukat, gula/madu, susu cair, dan es batu ke blender.";
